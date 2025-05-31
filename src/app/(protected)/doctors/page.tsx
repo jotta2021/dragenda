321cbd3235b10951d8e43fe508766e0121743f92
jotta2021 <joanderson2572@gmail.com>
2025-05-30 21:16:47 -0300

    feat: implement Doctors page layout with header, subtitle, and add button for adding new doctors

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,9 +1,34 @@
+import { Button } from '@/components/ui/button';
+import { PageActions, PageContainer, PageContent, PageHeader, PageHeaderContent, PageSubtitle, PageTitle } from '@/components/ui/page-container';
+import { Plus } from 'lucide-react';
 import React from 'react';
 
 // import { Container } from './styles';
 
 const Doctors = () => {
-  return <div />;
-}
+  return (
+ 
+      <PageContainer>
+        <PageHeader>
+          <PageHeaderContent>
+          <PageTitle><h1>Médicos</h1></PageTitle>
+          <PageSubtitle>Gerencie os médicos da sua clínica</PageSubtitle>
+          </PageHeaderContent>
+          <PageActions>
+          <Button>
+            <Plus  />
+            Adicionar Médico</Button>
+        </PageActions>
+        </PageHeader>
+        
+        <PageContent>
+<div>
+medicos
 
+</div>
+        </PageContent>
+      </PageContainer>
+  
+  )
+}
 export default Doctors;
---
+++ src/components/ui/page-container.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export const  PageContainer  = ({children}:{children:React.ReactNode})=> {
+return <div className='space-y-6 p-6 w-full'>{children}</div>
+}
+
+export const PageHeader =({children}:{children:React.ReactNode})=> {
+return <div className='flex items-center justify-between'>{children}</div>
+}
+export const PageHeaderContent =({children}:{children:React.ReactNode})=> {
+    return <div className='flex flex-col'>{children}</div>
+    }
+
+export const PageTitle = ({children}:{children:React.ReactNode})=> {
+return <h1 className='text-2xl font-bold'>{children}</h1>
+}
+
+export const PageSubtitle = ({children}:{children:React.ReactNode})=> {
+return <p className='text-sm text-muted-foreground'>{children}</p>
+}
+
+export const PageContent = ({children}:{children:React.ReactNode})=> {
+return <div className='space-y-6'>{children}</div>
+}
+
+export const PageActions = ({children}:{children:React.ReactNode})=> {
+return <div className='flex items-center '>{children}</div>
+}
+
+
